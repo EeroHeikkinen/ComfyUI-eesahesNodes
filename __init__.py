31f9480b5f5d10a842e9f485ff94a085db1d9c55
Eero Heikkinen <eero.heikkinen@gmail.com>
2024-08-18 03:36:44 +0000

    Add metadata

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+"""
+@author: eesahe
+@title: eesahe's Nodes
+@nickname: eesahesNodes
+@description: eesahe's Nodes: InstantX's Flux union ControlNet loader and implementation
+"""
+
 from .nodes import InstantXFluxUnionControlNetLoader
 
 NODE_CLASS_MAPPINGS = { 
