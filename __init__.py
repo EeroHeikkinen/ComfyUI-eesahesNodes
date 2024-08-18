d1700e1b610a5134267acf94223c6d99627b00ed
Xrvk <eero.heikkinen@gmail.com>
2024-08-18 06:49:31 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 @author: eesahe
 @title: eesahe's Nodes
 @nickname: eesahesNodes
-@description: eesahe's Nodes: InstantX's Flux union ControlNet loader and implementation
+@description: InstantX's Flux union ControlNet loader and implementation
 """
 
 from .nodes import InstantXFluxUnionControlNetLoader
